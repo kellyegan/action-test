347444c66d5ef9b1c535c1c7533bf92145f8e2c6
Kelly Egan <kelly.egan@gmail.com>
2020-06-25 16:27:42 -0400

    write to public/index.html

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
+const fs = require('fs');
 
 console.log("Hello github actions!");
+
+fs.writeFile("public/index.html", `<h1>${Date.now()}</h1>`, (error) => {
+  if(error) {
+    return console.log(error);
+  }
+});
