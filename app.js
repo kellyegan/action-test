2a382a0ca1e311d51a90ba0d309f487e7b91d570
Kelly Egan <kelly.egan@gmail.com>
2020-06-25 16:47:00 -0400

    Remove extra right parenthesis

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 console.log("Hello github actions!");
 
 getDate = () => {
-  return new Intl.DateTimeFormat('en-US').format(Date.now()))
+  return new Intl.DateTimeFormat('en-US').format(Date.now());
 }
 
 fs.writeFile("public/index.html", `<h1>The date is ${getDate()}</h1>`, (error) => {
